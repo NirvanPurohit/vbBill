7af076a398b2a73b855f59b84a608717a00ad4c8
NirvanPurohit <nirvanpurohit5@gmail.com>
2025-04-12 17:58:18 +0530

    initial commit

---
+++ frontend/vbBill/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div className="flex items-center justify-center h-full">
+        <h1 className="text-4xl font-semibold text-gray-800 tracking-wide font-sans">
+          VB-BILLING SYSTEM
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
